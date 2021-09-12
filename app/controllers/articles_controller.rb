16b459e7c759112156c4fae0417cd832d911de50
chucoyos <facturasmanzanillo@hotmail.com>
2021-09-12 04:15:11 -0500

    add edit and update actions and edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -19,6 +23,17 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end    
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if  @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+        
     end
 
     private
