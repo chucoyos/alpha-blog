4807018d84aecfb0007af85de0bf07e3bcb6a749
chucoyos <facturasmanzanillo@hotmail.com>
2022-03-10 23:40:16 -0600

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ApplicationRecord
+	validates :username,
+	          presence: true,
+	          uniqueness: {
+			case_sensitive: false
+	          },
+	          length: {
+			minimum: 3,
+			maximum: 25
+	          }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email,
+	          presence: true,
+	          uniqueness: true,
+	          length: {
+			minimun: 10,
+			maximum: 155
+	          },
+	          format: {
+			with: VALID_EMAIL_REGEX
+	          }
+
+	has_many :articles
 end
