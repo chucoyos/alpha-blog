addd9df5afe4ceb076c53b8cdcdcaa5f03f64c4a
chucoyos <facturasmanzanillo@hotmail.com>
2022-03-21 02:15:21 -0600

    add users signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  resources :users
+	root to: 'articles#index'
+
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :articles, only: %i[show index new create edit update destroy]
+	resources :users, except: %i[new, index]
+	get 'signup', to: 'users#new'
+	get 'users', to: 'users#new'
 end
