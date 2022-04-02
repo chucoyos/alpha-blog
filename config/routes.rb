21a46936a0f5b8425021966bc8fdb6a2330aea66
chucoyos <facturasmanzanillo@hotmail.com>
2022-04-01 23:36:25 -0600

    add user partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	resources :articles, only: %i[show index new create edit update destroy]
-	resources :users, except: %i[new, index]
+	resources :users, except: %i[new]
 	get 'signup', to: 'users#new'
-	get 'users', to: 'users#new'
 end
