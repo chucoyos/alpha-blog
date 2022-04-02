f60ef717e8c9f1dba515d62df588c58f6218122b
chucoyos <facturasmanzanillo@hotmail.com>
2022-04-02 12:37:46 -0600

    add will_paginate and set styles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,7 @@ class ArticlesController < ApplicationController
 	def show; end
 
 	def index
-		@articles = Article.all
-		@user = User.first
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
