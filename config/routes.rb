a205c38b19e88078372df794d2703cf3f6123998
chucoyos <facturasmanzanillo@hotmail.com>
2022-04-06 00:22:48 -0500

    set login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 	resources :articles, only: %i[show index new create edit update destroy]
 	resources :users, except: %i[new]
 	get 'signup', to: 'users#new'
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
 end
