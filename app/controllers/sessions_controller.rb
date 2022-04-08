97092b0a92eea794cf751c5a224a50eaa58063c7
chucoyos <facturasmanzanillo@hotmail.com>
2022-04-07 22:01:58 -0500

    create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in user
+			flash[:success] = "You have successfully logged in #{user.username}"
+			session[:user_id] = user.id
+
+			#sign_in user
 			redirect_to user
 		else
 			flash.now[:error] = 'Invalid email/password combination'
@@ -13,7 +16,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		sign_out
+		session[:user_id] = nil
+		flash[:success] = 'You have successfully logged out'
 		redirect_to root_url
 	end
 end
